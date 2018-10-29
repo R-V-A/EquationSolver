129e9a012967d66bac96bf72c15f70a82659b176
Brusensky V.B <vlad98shefpovar@gmail.com>
2018-10-29 16:36:23 +0200

    Add project files.

---
+++ Eq5/Computing.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Eq5
+{
+    class Computing
+    {
+        public List<double> A;
+        public static byte Power;
+        private static int _f;
+        private static double[,] _intervals = new double[5, 2];
+        public static double[] Coefficients;
+        public static List<string> Roots = new List<string>();
+
+        public Computing(List<double> A, byte power)
+        {
+            this.A = A;
+            Power = power;
+        }
+
+        public static void GetSolveSecondPowEq()
+        {
+            double D;
+            Roots.Clear();
+            var a = Coefficients[2];
+            var b = Coefficients[1];
+            var c = Coefficients[0];
+            D = b * b - 4 * a * c;
+            if (D<0)
+            {
+                var x1 = (-b / (2 * a)).ToString() +" + "+ (Math.Sqrt(Math.Abs(D))/(2*a)).ToString() + "i";
+                var x2 = (-b / (2 * a)).ToString() +" - "+ (Math.Sqrt(Math.Abs(D)) / (2 * a)).ToString() + "i";
+                Roots.Add(x1);
+                Roots.Add(x2);
+            }
+            else
+            { 
+                var x1 = ((-b + Math.Sqrt(D)) / (2 * a)).ToString();
+                var x2 = ((-b - Math.Sqrt(D)) / (2 * a)).ToString();
+                Roots.Add(x1);
+                Roots.Add(x2);
+            }
+        }
+
+
+        public static void GetSolveThirdPowEq()
+        {
+            Roots.Clear();
+            var a = Coefficients[2]/Coefficients[3];
+            var b = Coefficients[1]/Coefficients[3];
+            var c = Coefficients[0]/Coefficients[3];
+            var Q = (a*a-3*b)/9;
+            var R = (2*a*a*a-9*a*b+27*c)/54;
+            if (R*R<Q*Q*Q)
+            {
+                var t = Math.Acos(R / Math.Sqrt(Math.Pow(Q, 3))) / 3;
+                var x1 = -2 * Math.Sqrt(Q) * Math.Cos(t) - a / 3;
+                var x2 = -2 * Math.Sqrt(Q) * Math.Cos(t + (2 * Math.PI / 3)) - a / 3;
+                var x3 = -2 * Math.Sqrt(Q) * Math.Cos(t - (2 * Math.PI / 3)) - a / 3;
+                Roots.Add(x1.ToString());
+                Roots.Add(x2.ToString());
+                Roots.Add(x3.ToString());
+            }
+
+            if (Math.Pow(R,2)>=Math.Pow(Q,3))
+            {
+                var A = -1 * Math.Sign(R) * Math.Pow(Math.Abs(R) + Math.Sqrt(Math.Pow(R, 2) - Math.Pow(Q, 3)),1.0/3);
+                var B = 0.0;
+                if (A != 0)
+                    B = Q / A;
+                var x1 = (A + B) - a / 3;
+                var x2 = -(A + B) / 2 - a / 3 + (Math.Sqrt(3) * (A - B) / 2).ToString() + "i";
+                if (A == B)
+                    x2 = (- A - a / 3 ).ToString();
+                var x3 = x2;
+                Roots.Add(x1.ToString());
+                Roots.Add(x2);
+                Roots.Add(x3);
+            }
+        }
+
+        public static void GetSolveFourthPowEq()
+        {
+            
+        }
+
+        public static double[,] GetIntervals(double a, double b) 
+        {
+
+            _f = 0;
+            double begin, end;
+            while (a <= b)
+            {
+                begin = a;
+                end = begin + 0.1;
+                if (F(begin) * F(end) <= 0)
+                {
+                    _intervals[_f, 0] = Math.Floor(begin);
+                    if (begin * end < 0)
+                        _intervals[_f, 0] = Math.Ceiling(begin);
+                    _intervals[_f, 1] = Math.Ceiling(end);
+                    _f++;
+                }
+                a+=0.1;
+            }
+            return _intervals;
+        }
+        
+        private static double F(double X)
+        {
+            double result = 0;
+            var power = 0;
+            foreach (var coef in Coefficients)
+            {
+                result += coef * Math.Pow(X, power);
+                power++;
+            }
+            return result;
+
+        }
+
+        #region Метод половинного деления
+        public static void HalfDivision(double eps)
+        {
+            Roots.Clear();
+            for (int i = 0; i < _f; i++)
+            {
+                var a = _intervals[i,0];
+                var b = _intervals[i,1];
+                double c;
+                for (;;)
+                {
+                    var fa = F(a);
+                    c = (a + b) / 2;
+                    var fc = F(c);
+                    if (Math.Abs(fc) > eps || Math.Abs(b - a) >= eps)
+                        if (fa * fc > 0)
+                            a = c;
+                        else
+                            b = c;
+                    else
+                    {
+                        Roots.Add(Math.Round(c,7).ToString());
+                        break;
+                    }
+                }
+            }
+        }
+        #endregion
+        public double FxP(double x)
+        {
+            switch (Power)
+            {
+                case 1: return A[1] * x + A[0];
+                case 2: return A[2] * Math.Pow(x, 2) + A[1] * x + A[0];
+                case 3: return A[3] * Math.Pow(x, 3) + A[2] * Math.Pow(x, 2) + A[1] * x + A[0];
+                case 4: return A[4] * Math.Pow(x, 4) + A[3] * Math.Pow(x, 3) + A[2] * Math.Pow(x, 2) + A[1] * x + A[1];
+                case 5: return A[5] * Math.Pow(x, 5) + A[4] * Math.Pow(x, 4) + A[3] * Math.Pow(x, 3) + A[2] * Math.Pow(x, 2) + A[1] * x + A[0];
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Eq5/MainWindow.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Reporting;
+using OxyPlot.Series;
+//using OxyPlot.Wpf;
+
+namespace Eq5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBox[] arrayBoxs = { CoeffA0, CoeffA1, CoeffA2, CoeffA3, CoeffA4, CoeffA5 };
+            Label[] arrayLabels = { CoeffLabelA0, CoeffLabelA1, CoeffLabelA2, CoeffLabelA3, CoeffLabelA4, CoeffLabelA5 };
+            for (int j = 0; j <= 5; j++)
+            {
+                arrayBoxs[j].Visibility = Visibility.Collapsed;
+                arrayLabels[j].Visibility = Visibility.Collapsed;
+            }
+
+        }
+        public void Pop_DiAnswers()
+        {
+            TextBlock[] answersFields = { DigitalRootX1, DigitalRootX2, DigitalRootX3, DigitalRootX4, DigitalRootX5 };
+            var roots = Computing.Roots;
+            var i = 0;
+            foreach (var root in roots)
+            {
+                answersFields[i].Text = root;
+                i++;
+            }
+        }
+        public void Pop_AnAnswers()
+        {
+            TextBlock[] answersFields = { AnalizeRootX1, AnalizeRootX2, AnalizeRootX3, AnalizeRootX4, AnalizeRootX5 };
+            var roots = Computing.Roots;
+            var i = 0;
+            foreach (var root in roots)
+            {
+                answersFields[i].Text = root;
+                i++;
+            }
+        }
+
+        private void GetSolveA_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Computing.Coefficients = new[]
+                {
+                    Convert.ToDouble(CoeffA0.Text), Convert.ToDouble(CoeffA1.Text), Convert.ToDouble(CoeffA2.Text),
+                    Convert.ToDouble(CoeffA3.Text), Convert.ToDouble(CoeffA4.Text), Convert.ToDouble(CoeffA5.Text)
+                };
+                switch (Convert.ToByte(PowerComboBox.Text))
+                {
+                    case 2:
+                        Computing.GetSolveSecondPowEq();
+                        Pop_AnAnswers();
+                        break;
+                    case 3:
+                        Computing.GetSolveThirdPowEq();
+                        Pop_AnAnswers();
+                        break;
+                    case 4: //Computing.GetSolveFourthPowEq();
+                    //break;
+                    case 5:
+                        MessageBox.Show("Тут только численные решения :(");
+                        break;
+                }
+            }
+
+            catch (FormatException exception)
+            {
+                MessageBox.Show("Проверьте правильно ли введены степень и коэффициенты?");
+            }
+        }
+
+        private void GetSolveD_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Computing.Coefficients = new[]
+                {
+                    Convert.ToDouble(CoeffA0.Text), Convert.ToDouble(CoeffA1.Text), Convert.ToDouble(CoeffA2.Text),
+                    Convert.ToDouble(CoeffA3.Text), Convert.ToDouble(CoeffA4.Text), Convert.ToDouble(CoeffA5.Text)
+                };
+            }
+            catch (FormatException exception)
+            {
+                MessageBox.Show("Что-то странные коэффициенты.");
+            }
+            Computing.GetIntervals(Convert.ToDouble(IntervalA.Text), Convert.ToDouble(IntervalB.Text));
+            Computing.HalfDivision(Convert.ToDouble(Accurate.Text));
+            Pop_DiAnswers();
+            MessageBox.Show("Digital Solve!");
+        }
+
+        private void GetGraph_OnClick(object sender, RoutedEventArgs e)
+        {
+            var power = Convert.ToByte(PowerComboBox.Text);
+            TextBox[] textBoxs = {CoeffA0, CoeffA1, CoeffA2, CoeffA3, CoeffA4, CoeffA5};
+            List<double> A = new List<double>();
+            for (int k = 0; k <= power; k++)
+            {
+                A.Add(Convert.ToDouble(textBoxs[k].Text));
+            }
+            Computing T = new Computing(A,power);
+            var newModel = new PlotModel();
+            double xMin, xMax, yMin, yMax;
+            xMax = Convert.ToDouble(XMAX.Text);
+            xMin = Convert.ToDouble(XMIN.Text);
+            yMax = Convert.ToDouble(YMAX.Text);
+            yMin = Convert.ToDouble(YMIN.Text);
+            newModel.Axes.Clear();
+            newModel.Axes.Add(new LinearAxis()
+            {
+                Position = AxisPosition.Left,
+                PositionAtZeroCrossing = true,
+                AxislineStyle = LineStyle.Solid,
+                FilterMaxValue = xMax,
+                FilterMinValue = xMin
+            });
+            newModel.Axes.Add(new LinearAxis()
+            {
+                Position = AxisPosition.Bottom,
+                PositionAtZeroCrossing = true,
+                AxislineStyle = LineStyle.Solid,
+                FilterMaxValue = yMax,
+                FilterMinValue = yMin
+            });
+            newModel.Series.Add(new FunctionSeries(T.FxP,xMin,xMax,0.1));
+            PlotView.Model = newModel;
+            MessageBox.Show("There will be a graph");
+        }
+        
+        private void PowerComboBox_OnDropDownClosed(object sender, EventArgs e)
+        {
+            TextBox[] arrayBoxs = { CoeffA0, CoeffA1, CoeffA2, CoeffA3, CoeffA4, CoeffA5 };
+            Label[] arrayLabels = { CoeffLabelA0, CoeffLabelA1, CoeffLabelA2, CoeffLabelA3, CoeffLabelA4, CoeffLabelA5 };
+            int power;
+            try
+            {
+                power = Convert.ToInt16(PowerComboBox.Text);
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception);
+                power = 0;
+            }
+            switch (power)
+            {
+                case 0:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/PowerEmpty.png", UriKind.Relative));
+                    break;
+                case 1:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/Power1.png", UriKind.Relative));
+                    break;
+                case 2:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/Power2.png", UriKind.Relative));
+                    break;
+                case 3:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/Power3.png", UriKind.Relative));
+                    break;
+                case 4:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/Power4.png", UriKind.Relative));
+                    break;
+                case 5:
+                    ImagesBox.Source = new BitmapImage(new Uri("Images/Power5.png", UriKind.Relative));
+                    break;
+            }
+
+            if (power != 0)
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (j <= power)
+                    {
+                        arrayBoxs[j].Visibility = Visibility.Visible;
+                        arrayLabels[j].Visibility = Visibility.Visible;
+                    }
+
+                    if (j > power)
+                    {
+                        arrayBoxs[j].Visibility = Visibility.Collapsed;
+                        arrayBoxs[j].Text ="0";
+                        arrayLabels[j].Visibility = Visibility.Collapsed;
+                    }
+                }
+        }
+    }
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
